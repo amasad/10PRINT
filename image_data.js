0a4518f4ed67402e5b4d0078649aac87a4d47308
Amjad Masad <amjad.masad@gmail.com>
2013-04-14 02:48:54 -0400

    looper

--- image_data.js
+++ image_data.js
@@ -1,33 +1,34 @@
+/* jshint asi:true, strict:false */
+/* global looper:false */
 (function () {
-  /* jshint asi:true, strict:false */
   var GridSize = {width: 40, height: 25}
   var CharSize = 8
 
-  var canvas = document.getElementById("canvas-image-data")
+  var canvas = document.getElementById('canvas-image-data')
   canvas.width = 320
   canvas.height = 200
   var context = canvas.getContext('2d')
 
   var forward  = [ [0, 0, 0, 0, 0, 0, 1, 1]
-               , [0, 0, 0, 0, 0, 1, 1, 1]
-               , [0, 0, 0, 0, 1, 1, 1, 0]
-               , [0, 0, 0, 1, 1, 1, 0, 0]
-               , [0, 0, 1, 1, 1, 0, 0, 0]
-               , [0, 1, 1, 1, 0, 0, 0, 0]
-               , [1, 1, 1, 0, 0, 0, 0, 0]
-               , [1, 1, 0, 0, 0, 0, 0, 0] ]
+                 , [0, 0, 0, 0, 0, 1, 1, 1]
+                 , [0, 0, 0, 0, 1, 1, 1, 0]
+                 , [0, 0, 0, 1, 1, 1, 0, 0]
+                 , [0, 0, 1, 1, 1, 0, 0, 0]
+                 , [0, 1, 1, 1, 0, 0, 0, 0]
+                 , [1, 1, 1, 0, 0, 0, 0, 0]
+                 , [1, 1, 0, 0, 0, 0, 0, 0] ]
 
   var backward = [ [1, 1, 0, 0, 0, 0, 0, 0]
-               , [1, 1, 1, 0, 0, 0, 0, 0]
-               , [0, 1, 1, 1, 0, 0, 0, 0]
-               , [0, 0, 1, 1, 1, 0, 0, 0]
-               , [0, 0, 0, 1, 1, 1, 0, 0]
-               , [0, 0, 0, 0, 1, 1, 1, 0]
-               , [0, 0, 0, 0, 0, 1, 1, 1]
-               , [0, 0, 0, 0, 0, 0, 1, 1] ]
+                 , [1, 1, 1, 0, 0, 0, 0, 0]
+                 , [0, 1, 1, 1, 0, 0, 0, 0]
+                 , [0, 0, 1, 1, 1, 0, 0, 0]
+                 , [0, 0, 0, 1, 1, 1, 0, 0]
+                 , [0, 0, 0, 0, 1, 1, 1, 0]
+                 , [0, 0, 0, 0, 0, 1, 1, 1]
+                 , [0, 0, 0, 0, 0, 0, 1, 1] ]
 
-  function generateImageData (opts) {
-    var matrix = opts.backslash ? backward : forward
+  function generateImageData (backslash) {
+    var matrix = backslash ? backward : forward
     var imageData = context.createImageData(CharSize, CharSize)
     var dptr = 0
     for (var y = 0; y < matrix.length; y++) {
@@ -45,31 +46,17 @@
     return imageData
   }
 
-  var backslash = generateImageData({backslash: true})
-  var forwslash = generateImageData({backslash: false})
+  var backslash = generateImageData(true)
+  var forwslash = generateImageData(false)
 
   function draw (data, x, y) {
     context.putImageData(data, x * CharSize, y * CharSize)
   }
 
-  function loop (x, y) {
+  looper(canvas, function (slash, x, y) {
     draw(
-      Math.floor(Math.random() * 2) ? backslash : forwslash, x, y
-    );
-    x++
-    if (x > 39) {
-      x = 0
-      y++
-    }
-    if (y > 24) {
-      var data = context.getImageData(0, 16, 320, 23 * 8)
-      canvas.width = canvas.width
-      context.putImageData(data, 0, 0)
-      y = 23
-    }
-    setTimeout(loop.bind(null, x, y), 10)
-  }
-
-  loop(0, 0)
+      slash === 'forward' ? forwslash : backslash, x, y
+    )
+  });
 
 })()
---
+++ looper.js
@@ -0,0 +1,22 @@
+/* jshint asi:true, strict:false */
+var looper = function (canvas, draw) {
+  var context = canvas.getContext('2d')
+  function loop (x, y) {
+    draw(
+      Math.floor(Math.random() * 2) ? 'forward' : 'backward', x, y
+    )
+    x++
+    if (x > 39) {
+      x = 0
+      y++
+    }
+    if (y > 24) {
+      var data = context.getImageData(0, 16, 320, 23 * 8)
+      canvas.width = canvas.width
+      context.putImageData(data, 0, 0)
+      y = 23
+    }
+    setTimeout(loop.bind(null, x, y), 3)
+  }
+  loop(0, 0)
+}
