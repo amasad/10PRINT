8dd734e4f672d37925ae0df78d2e9f0bf3e4d709
Amjad Masad <amjad.masad@gmail.com>
2013-04-21 02:44:40 -0400

    reorg

--- maze_walker.js
+++ maze_walker.js
@@ -5,43 +5,44 @@ this.createMazeWalker = function (canvas, GridSize, CharSize) {
   var up = {up: null}
   var down = {down: null}
 
-  function and (data1, data2) {
-    var ret = context.createImageData(CharSize, CharSize)
-    var arr1 = data1.data
-    var arr2 = data2.data
-    for (var i = 0; i < arr1.length; i++) {
-      ret.data[i] = arr1[i] || arr2[i]
-    }
-    return ret
-  }
-
-  function getChar (x, y) {
-    var imageData = context.getImageData(x * CharSize, y * CharSize, CharSize, CharSize)
-    return imageData.data[0] === 0 ? 'forward' : 'backward'
-  }
-
-  function drawWalker (x, y, ch, dir) {
-    var imageData;
-    if (ch === 'forward') {
-      if (dir === right || dir === down) {
-        imageData = mazeImages.walker.forwardUp
-      } else {
-        imageData = mazeImages.walker.forwardDown
-      }
-    }
-    if (ch === 'backward') {
-      if (dir === down || dir === left) {
-        imageData = mazeImages.walker.backwardUp
-      } else {
-        imageData = mazeImages.walker.backwardDown
-      }
-    }
-    var currImage = context.getImageData(x * CharSize, y * CharSize, CharSize, CharSize)
-    imageData = and(imageData, currImage)
-    context.putImageData(imageData, x * CharSize, y * CharSize)
-  }
-
   function createWalker (done) {
+
+    function and (data1, data2) {
+      var ret = context.createImageData(CharSize, CharSize)
+      var arr1 = data1.data
+      var arr2 = data2.data
+      for (var i = 0; i < arr1.length; i++) {
+        ret.data[i] = arr1[i] || arr2[i]
+      }
+      return ret
+    }
+
+    function getChar (x, y) {
+      var imageData = context.getImageData(x * CharSize, y * CharSize, CharSize, CharSize)
+      return imageData.data[0] === 0 ? 'forward' : 'backward'
+    }
+
+    function drawWalker (x, y, ch, dir) {
+      var imageData;
+      if (ch === 'forward') {
+        if (dir === right || dir === down) {
+          imageData = mazeImages.walker.forwardUp
+        } else {
+          imageData = mazeImages.walker.forwardDown
+        }
+      }
+      if (ch === 'backward') {
+        if (dir === down || dir === left) {
+          imageData = mazeImages.walker.backwardUp
+        } else {
+          imageData = mazeImages.walker.backwardDown
+        }
+      }
+      var currImage = context.getImageData(x * CharSize, y * CharSize, CharSize, CharSize)
+      imageData = and(imageData, currImage)
+      context.putImageData(imageData, x * CharSize, y * CharSize)
+    }
+
     return function walk (x, y, dir) {
       switch (dir) {
         case right:
